a6742df6a5e1a06016703ab1639786c23e8f0256
StudyBuddy888 <lorecafe888@gmail.com>
2025-08-18 20:03:06 +0530

    implemented core configuration

---
+++ app/core/config.py
@@ -0,0 +1,67 @@
+import os
+import secrets
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from pydantic.networks import PostgresDsn
+from pydantic import field_validator, ValidationInfo
+
+class Settings(BaseSettings):
+    DEBUG: bool = True
+
+    API_PREFIX: str = "/api"
+    JWT_SECRET_KEY: str = secrets.token_urlsafe(32)
+    JWT_EXPIRE: int = 60 * 24 * 8
+
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5433
+    DB_DATABASE: str = "main"
+    DB_USERNAME: str = "main"
+    DB_PASSWORD: str = "main"
+    DATABASE_URL: PostgresDsn | None = None
+    DATABASE_RO_URL: PostgresDsn | None = None
+
+    #construct DB URL
+    @field_validator("DATABASE_URL")
+    def assemble_db_connection(cls, v: str | None, info: ValidationInfo) -> Any:
+        if isinstance(v, str):
+            return v
+
+        return PostgresDsn(
+            f"""postgresql+psycopg://{info.data["DB_USERNAME"]}:{info.data["DB_PASSWORD"]}@
+                {info.data["DB_HOST"]}:{info.data["DB_PORT"]}/{info.data["DB_DATABASE"]}"""
+        )
+    
+    #ease switch between .env and .env.testing
+    model_config = SettingsConfigDict(
+        env_file=(".env.testing" if os.getenv("PYTHON_ENVIRONNEMENT") == "testing" else ".env")
+    )
+
+
+#readonly settings
+class SettingsReadOnly(BaseSettings):
+    DB_HOST: str = "localhost"
+    DB_RO_HOST: str | None = None
+    DB_PORT: int = 5433
+    DB_DATABASE: str = "main"
+    DB_USERNAME: str = "main"
+    DB_PASSWORD: str = "main"
+    DATABASE_RO_URL: PostgresDsn | None = None
+
+    @field_validator("DATABASE_RO_URL")
+    def assemble_db_connection(cls, v: str | None, info: ValidationInfo) -> Any:
+        if isinstance(v, str):
+            return v
+
+        return PostgresDsn(
+            f"""postgresql+psycopg://{info.data["DB_USERNAME"]}:{info.data["DB_PASSWORD"]}@
+            {info.data["DB_HOST"] or info.data["DB_RO_HOST"]}:{info.data["DB_PORT"]}/{info.data["DB_DATABASE"]}"""
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=(".env.testing" if os.getenv("PYTHON_ENVIRONNEMENT") == "testing" else ".env"),
+        extra="allow",
+    )
+
+settings = Settings()
+settingsReadOnly = SettingsReadOnly()
--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# - FastAPI auto-generates docs, but this lets you override metadata like title, version, etc
+from fastapi.openapi.utils import get_openapi 
+#import router
+#create a core config and import
+from app.core.config import Settings
+app = FastAPI() #enable debug, docs, etc  
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #allow_origins=["https://myfrontend.com"]
+ # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+    )
+
+app.include_router( )
+
+#custom schema
+app.openapi_schema  = get_openapi(
+    title="Practice API",
+    version="1.0.0",
+    description="This is a practice API for learning purposes.",
+    contact={
+        "name": "PADMACHARAN M", "email": "charangowda.m9141@gmail.com"},
+    routes=app.routes,
+    servers=[
+        {"url": "/api"}] # Add server URL if needed
+)
+
